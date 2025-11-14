3585e1d6a332ecedd1e5e11985373e65d7ab206c
andrew atkinson <blind.sam@gmail.com>
2025-11-14 12:54:42 -0800

    minor: adds a key prop

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,32 @@
-import React, { Component } from 'react';
-import './index.css';
-import Project from './Project';
-import state from './state'
+import React, { Component } from "react";
+import "./index.css";
+import Project from "./Project";
+import state from "./state";
 
 class App extends Component {
   constructor() {
-    super()
-    this.state = state
+    super();
+    this.state = state;
   }
 
   render() {
     return (
       <div>
         <div id="content">
-          {
-            this.state && this.state.sections.map(section =>
-              (
-                <Project  image={section.image}
-                          color={section.color}
-                          title={section.title}
-                          body={section.body}
-                          links={section.links} />
-              )
-            )
-          }
-        </div >
-        <div className="title">
-          {this.state.title}
+          {this.state &&
+            this.state.sections.map((section, idx) => (
+              <Project
+                image={section.image}
+                color={section.color}
+                title={section.title}
+                body={section.body}
+                links={section.links}
+                key={idx}
+              />
+            ))}
         </div>
-      </div >
+        <div className="title">{this.state.title}</div>
+      </div>
     );
   }
 }
