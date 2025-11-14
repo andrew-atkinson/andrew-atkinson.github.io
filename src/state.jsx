62679fabf84d9ae0ecdcb5c17c9912db9acd4bfa
andrew atkinson <blind.sam@gmail.com>
2025-11-14 11:52:08 -0800

    update: js to jsx

---
+++ src/state.jsx
@@ -0,0 +1,76 @@
+export default {
+  title: "Andrew Atkinson Projects",
+  sections: [
+    {
+      image: "img/nimbus.jpg",
+      color: "white",
+      title: "NIMBUS, INC",
+      body: [
+        `A hypothetical e-commerce site selling unusual weather`,
+        `PostgreSQL maintains ‘inventory’ with Sequelize and Express, Node serves the project with React, Redux and Bootstrap giving a seamless user experience, with OAuth and sessions providing authentication. Created db schemas, Express routes for products, front-end through bootstrap, and managed the project through git and Waffle.io`,
+      ],
+      links: [
+        {
+          href: `https://github.com/nimbusInc/cloudslanger`,
+          icon: `fa-github`,
+          name: `Source`,
+        },
+        {
+          href: `https://nimbus-inc.herokuapp.com`,
+          icon: `fa-external-link`,
+          name: `Deploy`,
+        },
+      ],
+    },
+    {
+      image: "img/2020logo.jpg",
+      title: "20-20",
+      body: [
+        `Media availability can broaden our perspectives. However, with advancements in consumer targeting, news organizations have increasingly directed politics. 20-20 aggregates and provides comprehensive information, showcasing content in a web application and chrome extension.`,
+      ],
+      links: [
+        {
+          href: `https://github.com/20-20/twentytwenty`,
+          icon: `fa-github`,
+          name: `Source`,
+        },
+      ],
+    },
+    {
+      image: "img/comp-1-esja-4+5.jpg",
+      title: "PHOTOGRAPHY",
+      body: [
+        `A site dedicated to photographic projects on Iceland and whaling, spaces underneath NYC highways, and others.`,
+      ],
+      links: [
+        {
+          href: `http://andrewatkinson.net`,
+          icon: `fa-external-link`,
+          name: `Website`,
+        },
+      ],
+    },
+    {
+      image: "img/about.jpg",
+      title: "ABOUT",
+      body: [
+        `Art educator turned software developer. As an educator I direct the MFA Studio Art program where I mentor students through an intensive two-year process from conceptualization, through development and to thesis exhibition with printed monographs. I also co-authored the rewrite for the Art and Design Department’s degrees. The new degrees integrate art/design-focused digital technologies to update the curriculum, resulting in a relevant and competitive program to benefit students entering the creative workforce today. This rewriting process revealed the exciting potential for creative problem solving in software development, which brought me to javascript development and Fullstack Academy.`,
+        `Proficient: JavaScript/ES6, Node, Express, React, Redux, PostgreSQL, Git/Github, HTML, Sequelize :: Knowledgeable: D3.js, CSS, Bootstrap, Mocha, Chai, Jasmine :: Some Familiarity: Sass, jQuery, Socket.io`,
+      ],
+      links: [
+        {
+          href: `mailto:blind.sam@gmail.com`,
+          icon: `fa-envelope`,
+        },
+        {
+          href: `https://www.linkedin.com/in/andrewpeteratkinson/`,
+          icon: `fa-linkedin-square`,
+        },
+        {
+          href: `https://twitter.com/andrewatkinson`,
+          icon: `fa-twitter`,
+        },
+      ],
+    },
+  ],
+};
